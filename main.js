6b148ffd1ca0e6aa4dcb468cda6d30647352e207
André Luiz Da Silva Estevão <andre.estevao@linx.com.br>
2025-09-23 00:07:54 -0300

    Adjust JS vite

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
 
 // Smooth scrolling and navigation
 document.addEventListener('DOMContentLoaded', function() {
